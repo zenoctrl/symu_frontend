901ee4791d30473d3544305b23a42d22f8932264
zenoctrl <azronebrian@gmail.com>
2024-08-27 19:30:53 +0300

    added event emitter

--- src/app/components/sales/sales.component.ts
+++ src/app/components/sales/sales.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { AfterSalesComponent } from './after-sales/after-sales.component';
 
 @Component({
   selector: 'app-sales',
   templateUrl: './sales.component.html',
-  styleUrls: ['./sales.component.scss']
+  styleUrls: ['./sales.component.scss'],
 })
 export class SalesComponent {
+  @ViewChild(AfterSalesComponent) afterSaleComponent!: AfterSalesComponent;
 
+  handleCompleteEvent() {
+    console.log('sale completed');
+    this.afterSaleComponent.getPhones();
+  }
 }
