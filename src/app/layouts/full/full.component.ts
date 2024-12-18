3d9a09a865c1704098fa7c90142429213f04a5fe
zenoctrl <102949828+zenoctrl@users.noreply.github.com>
2024-12-18 19:40:44 +0300

    added users menu

--- src/app/layouts/full/full.component.ts
+++ src/app/layouts/full/full.component.ts
@@ -76,14 +76,21 @@ export class FullComponent {
       icon: 'settings',
       menu: 'Admin',
     },
+    {
+      link: '/users',
+      icon: 'users',
+      menu: 'Users',
+    }
   ];
 
   getUser() {
     this.user = JSON.parse(sessionStorage.getItem('user') || '{}');
     const role = this.user.roleModel.roleName;
-    
+
     if (!role.toLowerCase().includes('director')) {
-      this.sidebarMenu = this.sidebarMenu.filter((menu: sidebarMenu) => !menu.link.includes('admin'));
+      this.sidebarMenu = this.sidebarMenu.filter(
+        (menu: sidebarMenu) => !menu.link.includes('admin')
+      );
     }
 
     if (sessionStorage.getItem('countries') == null) {
@@ -109,7 +116,6 @@ export class FullComponent {
     if (sessionStorage.getItem('stock-statuses') == null) {
       this.getStockStatuses();
     }
-    
   }
 
   logout() {
@@ -187,7 +193,6 @@ export class FullComponent {
       (res: any) => {
         if (res.statusCode == 0) {
           sessionStorage.setItem('models', JSON.stringify(res.data));
-          
         } else {
           this.getDeviceModels();
         }
@@ -204,7 +209,6 @@ export class FullComponent {
       (res: any) => {
         if (res.statusCode == 0) {
           sessionStorage.setItem('stock-statuses', JSON.stringify(res.data));
-          
         } else {
           this.getStockStatuses();
         }
